b2a76aa7371e63d8585431e823b8d432e722b0a9
Caio Dantas <caioaires@gmail.com>
2022-12-18 20:49:22 -0300

    deployments added

---
+++ webApp/payment-web/src/environments/environment.kubernetes.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  envVar: {
+    /**
+     * Add environment variables you want to retrieve from process
+     * PORT:4200,
+     * VAR_NAME: defaultValue
+     */
+    BACKEND_URL: "http://payment-api:8000"
+  }
+};
